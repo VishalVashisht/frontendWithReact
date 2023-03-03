65e1d247c743235cb019e66cae495c7be0331d48
VishalVashisht <vishalvashisht0852@gmail.com>
2023-03-03 08:04:43 +0530

    everything in functional component

--- src/Component/User.js
+++ src/Component/User.js
@@ -1,48 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Hindi from "./hindi";
 import English from "./english";
 
-let val = Math.floor((Math.random()*100) + 1);
+export default function User(){
+    let val = Math.floor((Math.random()*100) + 1);
+    var[count,setData]= useState(val);
+    var[greet,changeGreet]=useState(true);
 
-export default class User extends React.Component{
-    
-    constructor(){
-        super();
-        this.state = { 
-            count: val,
-            greet: true
-        }
-    }
-    
-    single(value){
-        this.setState({
-            count: value==1 ? this.state.count+1 : (this.state.count==0 ? 0:this.state.count-1)
-        })
-
-    }
-
-    changeLang = ()=>{
-        this.setState({
-            greet: !this.state.greet
-        })
-    }
-    
-    reset(){
-        this.setState({count:val})
-    }
-
-    render(){
-        return(
-            <>
-            <div class="hmmm">
-                {this.state.greet ? <English/> : <Hindi/>} 
-                <button onClick={()=>{this.changeLang()}}>Change Lang</button>
-                <h1>{this.state.count}</h1>
-                <button onClick={()=>this.single(1)}>Increase </button>
-                <button onClick={()=>this.single(0)}>Decrease </button>
-                <button onClick={()=>this.reset()}>Reset </button>
-                </div>
-            </>
+    function single(value){
+        setData(
+            count= value===1 ? count+1 : (count===0 ? 0:count-1)
         )
     }
+
+    function changeLang(){
+        changeGreet(
+            greet= !greet
+        )
+    }
+    
+   function reset(){
+        setData(count=val)
+    }
+
+
+    return(
+        <>
+        <div className="hmmm">
+            {greet ? <English/> : <Hindi/>} 
+            <button onClick={()=>changeLang()}>Change Lang</button>
+            <h1>{count}</h1>
+            <button onClick={()=>single(1)}>Increase </button>
+            <button onClick={()=>single(0)}>Decrease </button>
+            <button onClick={()=>reset()}>Reset </button>
+        </div>
+        </>
+    )
 }
+
+
+
+
